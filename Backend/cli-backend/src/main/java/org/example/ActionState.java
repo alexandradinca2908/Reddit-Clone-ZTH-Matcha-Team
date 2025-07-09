1567c1439182ff7df20303a8bee64aa15d2e3812
Alexandra Dinca <alexandradi2003@gmail.com>
2025-07-09 11:08:41 +0300

    Updating Menu to differentiate between logged in and not logged in states

--- Backend/cli-backend/src/main/java/org/example/ActionState.java
+++ Backend/cli-backend/src/main/java/org/example/ActionState.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 
 public class ActionState {
     public enum State {
-        NOT_LOGGED_IN,
-        LOGGED_IN,
+        MAIN_MENU,
         ON_FEED,
         ON_POST,
         ON_COMMENT,
@@ -16,9 +15,10 @@ public class ActionState {
         QUIT
     }
 
+    private boolean isLoggedIn = false;
     private final static UserService userService = UserService.getInstance();
     private final static PostService postService = new PostService();
-    State currentState = State.NOT_LOGGED_IN;
+    State currentState = State.MAIN_MENU;
     User user;
 
     public boolean executeAction() {
@@ -27,75 +27,88 @@ public class ActionState {
         String sanitizedInput;
 
         switch (currentState) {
-            case NOT_LOGGED_IN:
-                System.out.println("1. Login\n2. Register\n3. Show feed\n4. Quit" );
+            case MAIN_MENU:
+                if (!isLoggedIn) {
+                    System.out.println("1. Login\n2. Register\n3. Show feed\n4. Quit" );
 
-                option = scan.nextLine();
-                sanitizedInput = sanitizeInput(option);
+                    option = scan.nextLine();
+                    sanitizedInput = sanitizeInput(option);
 
-                if (sanitizedInput.equalsIgnoreCase("login")) {
-                    user = userService.userLoginCLI();
+                    if (sanitizedInput.equalsIgnoreCase("login")) {
+                        user = userService.userLoginCLI();
 
-                    //  Display login menu only if action was successful
-                    if (user != null) {
-                        changeState(State.LOGGED_IN);
+                        //  Display login menu only if action was successful
+                        if (user != null) {
+                            isLoggedIn = true;
+                        }
+                    } else if (sanitizedInput.equalsIgnoreCase("register")) {
+                        user = userService.userRegisterCLI();
+                    } else if (sanitizedInput.equalsIgnoreCase("show feed")) {
+                        postService.showFeed();
+                        changeState(State.ON_FEED);
+                    } else if (sanitizedInput.equalsIgnoreCase("quit")) {
+                        changeState(State.QUIT);
                     }
-                } else if (sanitizedInput.equalsIgnoreCase("register")) {
-                    user = userService.userRegisterCLI();
-                    changeState(State.LOGGED_IN);
-                } else if (sanitizedInput.equalsIgnoreCase("show feed")) {
-                    postService.showFeed();
-                    changeState(State.ON_FEED);
-                } else if (sanitizedInput.equalsIgnoreCase("quit")) {
-                    changeState(State.QUIT);
-                }
+                } else {
+                    System.out.println("""
+                            1. Show feed
+                            2. Create post
+                            3. Add comment
+                            4. Logout
+                            5. Delete Account
+                            6. Quit""");
 
-                break;
+                    option = scan.nextLine();
+                    sanitizedInput = sanitizeInput(option);
 
-            case LOGGED_IN:
-                System.out.println("""
-                        1. Show feed
-                        2. Create post
-                        3. Add comment
-                        4. Logout
-                        5. Delete Account
-                        6. Quit""");
-
-                option = scan.nextLine();
-                sanitizedInput = sanitizeInput(option);
-
-                if (sanitizedInput.equalsIgnoreCase("show feed")) {
-                    postService.showFeed();
-                    changeState(State.ON_FEED);
-                }  else if (sanitizedInput.equalsIgnoreCase("create post")) {
-                    postService.addPost(user.getUsername());
-                } else if (sanitizedInput.equalsIgnoreCase("logout")) {
-                    user = null;
-                    changeState(State.LOGOUT);
-                } else if (sanitizedInput.equalsIgnoreCase("delete account")) {
-                    userService.userDeleteCLI(this.user);
-                    changeState(State.NOT_LOGGED_IN);
-                } else if (sanitizedInput.equalsIgnoreCase("quit")) {
-                    changeState(State.QUIT);
+                    if (sanitizedInput.equalsIgnoreCase("show feed")) {
+                        postService.showFeed();
+                        changeState(State.ON_FEED);
+                    }  else if (sanitizedInput.equalsIgnoreCase("create post")) {
+                        postService.addPost(user.getUsername());
+                    } else if (sanitizedInput.equalsIgnoreCase("logout")) {
+                        user = null;
+                        changeState(State.LOGOUT);
+                    } else if (sanitizedInput.equalsIgnoreCase("delete account")) {
+                        userService.userDeleteCLI(this.user);
+                        isLoggedIn = false;
+                    } else if (sanitizedInput.equalsIgnoreCase("quit")) {
+                        changeState(State.QUIT);
+                    }
                 }
 
                 break;
 
             case ON_FEED:
-                System.out.println("1. Expand post\n2. Logout\n3. Quit");
+                if (!isLoggedIn) {
+                    System.out.println("1. Expand post\n2. Quit");
 
-                option = scan.nextLine();
-                sanitizedInput = sanitizeInput(option);
+                    option = scan.nextLine();
+                    sanitizedInput = sanitizeInput(option);
 
-                if (sanitizedInput.equalsIgnoreCase("expand post")) {
-                    postService.expandPost();
-                    changeState(State.ON_POST);
-                } else if (sanitizedInput.equalsIgnoreCase("logout")) {
-                    user = null;
-                    changeState(State.LOGOUT);
-                } else if (sanitizedInput.equalsIgnoreCase("quit")) {
-                    changeState(State.QUIT);
+                    if (sanitizedInput.equalsIgnoreCase("expand post")) {
+                        postService.expandPost();
+                        changeState(State.ON_POST);
+                    } else if (sanitizedInput.equalsIgnoreCase("quit")) {
+                        changeState(State.QUIT);
+                    }
+                } else {
+                    System.out.println("1. Expand post\n2. Logout\n3. Quit");
+
+                    option = scan.nextLine();
+                    sanitizedInput = sanitizeInput(option);
+
+                    if (sanitizedInput.equalsIgnoreCase("expand post")) {
+                        postService.expandPost();
+                        changeState(State.ON_POST);
+                    } else if (sanitizedInput.equalsIgnoreCase("logout")) {
+                        user = null;
+                        changeState(State.LOGOUT);
+                    } else if (sanitizedInput.equalsIgnoreCase("quit")) {
+                        changeState(State.QUIT);
+                    }
                 }
+
                 break;
 
             case ON_POST:
@@ -162,7 +175,7 @@ public class ActionState {
 
             case LOGOUT:
                 System.out.println("You have been logged out.");
-                changeState(State.NOT_LOGGED_IN);
+                isLoggedIn  = false;
                 break;
 
             case QUIT:
@@ -182,49 +195,60 @@ public class ActionState {
 
     private String sanitizeInput(String input) {
         switch (currentState) {
-            case NOT_LOGGED_IN:
-                // 1. Login, 2. Register, 3. Logout, 4. Quit
-                switch (input) {
-                    case "1":
-                        return "login";
+            case MAIN_MENU:
+                if (!isLoggedIn) {
+                    // 1. Login, 2. Register, 3. Logout, 4. Quit
+                    switch (input) {
+                        case "1":
+                            return "login";
 
-                    case "2":
-                        return "register";
+                        case "2":
+                            return "register";
 
-                    case "3":
-                        return "show feed";
+                        case "3":
+                            return "show feed";
+                    }
+                } else {
+                    //  1. Show feed, 2. Create post, 3. Add comment, 4. Logout, 5. Quit
+                    switch (input) {
+                        case "1":
+                            return "show feed";
 
-                    case "4":
-                        return "quit";
+                        case "2":
+                            return "create post";
+
+                        case "3":
+                            return "logout";
+                    }
                 }
 
-            case LOGGED_IN:
-                //  1. Show feed, 2. Create post, 3. Add comment, 4. Logout, 5. Quit
-                switch (input) {
-                    case "1":
-                        return "show feed";
-
-                    case "2":
-                        return "create post";
-
-                    case "3":
-                        return "logout";
-
-                    case "4":
-                        return "quit";
+                if (input.equals("4")) {
+                    return "quit";
                 }
 
             case ON_FEED:
-                //  1. Expand post, 2. Logout, 3. Quit
-                switch (input) {
-                    case "1":
-                        return "expand post";
+                if (!isLoggedIn) {
+                    //  1. Expand post, 2. Quit
+                    switch (input) {
+                        case "1":
+                            return "expand post";
 
-                    case "2":
-                        return "logout";
+                        case "2":
+                            return "quit";
+                    }
 
-                    case "3":
-                        return "quit";
+                } else {
+                    //  1. Expand post, 2. Logout, 3. Quit
+                    switch (input) {
+                        case "1":
+                            return "expand post";
+
+                        case "2":
+                            return "logout";
+
+                        case "3":
+                            return "quit";
+                    }
                 }
 
             case ON_POST:
@@ -254,7 +278,7 @@ public class ActionState {
                 }
 
             case ON_COMMENT:
-                //  1. Reply,2. Upvote, 3. Downvote,
+                //  1. Reply, 2. Upvote, 3. Downvote,
                 //  4. Return to post, 5. Logout, 6. Quit
                 switch (input) {
                     case "1":
