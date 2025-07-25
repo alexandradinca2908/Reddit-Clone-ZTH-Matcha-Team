8858c229a56ac867ef6b82f898b87dfe3bc9da92
Alexandra Dinca <alexandradi2003@gmail.com>
2025-07-25 10:57:48 +0300

    Modified Subreddit to match its entity

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/model/Subreddit.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/model/Subreddit.java
@@ -1,21 +1,26 @@
 package org.matcha.springbackend.model;
 
+import java.util.UUID;
+
 public class Subreddit {
-    private String id;
-    private String name;
+    private UUID id;
+    private Account account;
     private String displayName;
     private String description;
+    private boolean isDeleted;
     private Integer memberCount;
     private Integer postCount;
     private String iconUrl;
     private String createdAt;
 
-    public Subreddit(String id, String name, String displayName, String description,
-                     Integer memberCount, Integer postCount, String iconUrl, String createdAt) {
+    public Subreddit(UUID id, Account account, String displayName,
+                     String description, boolean isDeleted, Integer memberCount,
+                     Integer postCount, String iconUrl, String createdAt) {
         this.id = id;
-        this.name = name;
+        this.account = account;
         this.displayName = displayName;
         this.description = description;
+        this.isDeleted = isDeleted;
         this.memberCount = memberCount;
         this.postCount = postCount;
         this.iconUrl = iconUrl;
