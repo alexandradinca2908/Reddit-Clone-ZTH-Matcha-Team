accc02f3288db49d0cfdbf6f4b0f72f9559f9e86
Darius Preda <dariuspreda02@gmail.com>
2025-07-25 11:45:42 +0300

    Fixed refactoring BUG

--- Frontend/cli-frontend/src/main/java/org/example/models/Post.java
+++ Frontend/cli-frontend/src/main/java/org/example/models/Post.java
@@ -1,4 +1,4 @@
-package org.example.DTO;
+package org.example.models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- Frontend/cli-frontend/src/main/java/org/example/models/State.java
+++ Frontend/cli-frontend/src/main/java/org/example/models/State.java
@@ -1,4 +1,4 @@
-package org.example.DTO;
+package org.example.models;
 
 public enum State {
     MAIN_MENU,
