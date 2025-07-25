a3cc2cd1502ddb1924314ba1a121bee38557cde8
Alexandra Dinca <alexandradi2003@gmail.com>
2025-07-25 10:53:51 +0300

    Subreddit model + added helper classes for wrapping the responses

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/model/Subreddit.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/model/Subreddit.java
@@ -1,4 +1,24 @@
 package org.matcha.springbackend.model;
 
 public class Subreddit {
+    private String id;
+    private String name;
+    private String displayName;
+    private String description;
+    private Integer memberCount;
+    private Integer postCount;
+    private String iconUrl;
+    private String createdAt;
+
+    public Subreddit(String id, String name, String displayName, String description,
+                     Integer memberCount, Integer postCount, String iconUrl, String createdAt) {
+        this.id = id;
+        this.name = name;
+        this.displayName = displayName;
+        this.description = description;
+        this.memberCount = memberCount;
+        this.postCount = postCount;
+        this.iconUrl = iconUrl;
+        this.createdAt = createdAt;
+    }
 }
