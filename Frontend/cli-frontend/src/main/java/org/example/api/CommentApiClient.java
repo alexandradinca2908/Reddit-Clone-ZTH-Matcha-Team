e8e6b4a7e2a662eeb3e239e7be59462bf0b31704
Matthew64bit <matei.stoicea@gmail.com>
2025-07-25 13:52:38 +0300

    CommentApiClient work in progress.

--- Frontend/cli-frontend/src/main/java/org/example/api/CommentApiClient.java
+++ Frontend/cli-frontend/src/main/java/org/example/api/CommentApiClient.java
@@ -3,9 +3,15 @@ package org.example.api;
 import org.example.models.Comment;
 import org.example.models.Post;
 import org.example.models.User;
+import org.example.userinterface.UIComment;
+
+import java.util.Scanner;
 
 public class CommentApiClient extends BaseApiClient{
     private static CommentApiClient instance;
+    private static UIComment uiComment = UIComment.getInstance();
+    private final Scanner sc = new Scanner(System.in);
+
     private CommentApiClient(String baseUrl) {
         super(baseUrl);
     }
@@ -17,16 +23,29 @@ public class CommentApiClient extends BaseApiClient{
         return instance;
     }
 
+    //POST
     public void addComment(User user, Post post) {
+        uiComment.pleaseEnter("comment");
+        String input = sc.nextLine();
+        Comment comm = new Comment(post, user, input);
+
+        post.getCommentList().add(comm);
+        uiComment.addedSuccessfully("comment");
+
+        //TODO - Trebuie trimisa postarea cu comentariul adaugat - Se face in PostApiClient
     }
 
+    //GET
     public Comment selectReply(Comment currentComment) {
+        uiComment.pleaseEnter("replyID");
         return null;
     }
 
+    //POST
     public void addReply(User user, Comment comment) {
     }
 
+    //GET
     public Comment selectComment(Post post) {
         return null;
     }
