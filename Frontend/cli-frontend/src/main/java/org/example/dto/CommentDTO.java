ca3d79b88acda3e5dd30e28f31d816b754a67a7e
Darius Preda <dariuspreda02@gmail.com>
2025-07-25 11:24:37 +0300

    Started moving frontend in Spring

---
+++ Frontend/cli-frontend/src/main/java/org/example/dto/CommentDTO.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import java.util.List;
+
+public record CommentDTO(
+        String id,
+        String postId,
+        String parentId,
+        String content,
+        String author,
+        Integer upvotes,
+        Integer downvotes,
+        Integer score,
+        String userVote,
+        String createdAt,
+        String updatedAt,
+        List<CommentDTO> replies
+) {
+}
