480f7305abfeb24d29ff615927a55c8f32c4d3d6
Alexandra Dinca <alexandradi2003@gmail.com>
2025-07-28 14:50:23 +0300

    Hardcoded data for test in SpringApplicationBackend

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/SpringBackendApplication.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/SpringBackendApplication.java
@@ -1,12 +1,22 @@
 package org.matcha.springbackend;
 
+import org.matcha.springbackend.model.Account;
+import org.matcha.springbackend.model.Subreddit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @SpringBootApplication
 public class SpringBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBackendApplication.class, args);
+
+        //  DELETE WHEN DB IS POPULATED
+        Account account = new Account();
+        Subreddit subreddit = new Subreddit(UUID.randomUUID(), account, "echipa 1", "matchaaa", false,
+                1, 0, "", OffsetDateTime.now());
     }
 }
