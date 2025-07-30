3849e354984a14444c8980b6b03934b39b83f672
Alexandra Dinca <alexandradi2003@gmail.com>
2025-07-30 15:59:53 +0300

    Working on CommentController

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/controller/CommentController.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/controller/CommentController.java
@@ -1,16 +1,22 @@
 package org.matcha.springbackend.controller;
 
 import org.matcha.springbackend.dto.comment.CommentDTO;
+import org.matcha.springbackend.dto.post.PostDTO;
 import org.matcha.springbackend.mapper.AccountMapper;
 import org.matcha.springbackend.mapper.CommentMapper;
 import org.matcha.springbackend.mapper.PostMapper;
 import org.matcha.springbackend.mapper.VoteMapper;
+import org.matcha.springbackend.repositories.CommentRepository;
+import org.matcha.springbackend.response.DataResponse;
+import org.matcha.springbackend.service.CommentService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,16 +25,27 @@ public class CommentController {
     private final AccountMapper accountMapper;
     private final VoteMapper voteMapper;
     private final CommentMapper commentMapper;
+    private final CommentService commentService;
 
-    public CommentController(PostMapper postMapper, AccountMapper accountMapper, VoteMapper voteMapper, CommentMapper commentMapper) {
+    public CommentController(PostMapper postMapper, AccountMapper accountMapper, VoteMapper voteMapper,
+                             CommentMapper commentMapper, CommentService commentService) {
         this.postMapper = postMapper;
         this.accountMapper = accountMapper;
         this.voteMapper = voteMapper;
         this.commentMapper = commentMapper;
+        this.commentService = commentService;
     }
 
     @GetMapping("/{postId}/comments")
-    public List<CommentDTO> getComments(@PathVariable String postId) {
-        return null;
+    public ResponseEntity<DataResponse<List<CommentDTO>>> getCommentsFromPost(@PathVariable String postId) {
+        //  Map Comments to CommentDTOs
+        List<CommentDTO> commentDTOS = commentService.getCommentsByPostId(UUID.fromString(postId))
+                .stream()
+                .map(commentMapper::modelToDTO)
+                .toList();
+
+        DataResponse<List<CommentDTO>> dataResponse = new DataResponse<>(true, commentDTOS);
+        return ResponseEntity.ok(dataResponse);
     }
+
 }
---
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/service/CommentService.java
@@ -0,0 +1,37 @@
+package org.matcha.springbackend.service;
+
+import org.matcha.springbackend.dto.comment.CommentDTO;
+import org.matcha.springbackend.entities.CommentEntity;
+import org.matcha.springbackend.mapper.CommentMapper;
+import org.matcha.springbackend.model.Comment;
+import org.matcha.springbackend.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentMapper commentMapper, CommentRepository commentRepository) {
+        this.commentMapper = commentMapper;
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getCommentsByPostId(UUID postId) {
+        List<CommentEntity> entities = commentRepository.findByPostId(postId).orElse(new ArrayList<>());
+
+        return entities.stream()
+                .map(commentMapper::entityToModel)
+                .collect(Collectors.toList());
+    }
+
+    //  TODO
+    public List<Comment> organizeCommentsByHierarchy(List<Comment> comments) {
+        return null;
+    }
+}
