18cabd6c14520ee7e1fdd3a3284ffa4a16596db4
MihneaCucu <cucumihnea0@gmail.com>
2025-07-30 16:59:32 +0300

    Added postcomment

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/controller/CommentController.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/controller/CommentController.java
@@ -1,20 +1,21 @@
 package org.matcha.springbackend.controller;
 
 import org.matcha.springbackend.dto.comment.CommentDTO;
+import org.matcha.springbackend.dto.comment.requestbody.AddCommentBodyDTO;
 import org.matcha.springbackend.dto.post.PostDTO;
 import org.matcha.springbackend.mapper.AccountMapper;
 import org.matcha.springbackend.mapper.CommentMapper;
 import org.matcha.springbackend.mapper.PostMapper;
 import org.matcha.springbackend.mapper.VoteMapper;
+import org.matcha.springbackend.model.Comment;
 import org.matcha.springbackend.repositories.CommentRepository;
 import org.matcha.springbackend.response.DataResponse;
+import org.matcha.springbackend.service.AccountService;
 import org.matcha.springbackend.service.CommentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,14 +27,16 @@ public class CommentController {
     private final VoteMapper voteMapper;
     private final CommentMapper commentMapper;
     private final CommentService commentService;
+    private final AccountService accountService;
 
     public CommentController(PostMapper postMapper, AccountMapper accountMapper, VoteMapper voteMapper,
-                             CommentMapper commentMapper, CommentService commentService) {
+                             CommentMapper commentMapper, CommentService commentService, AccountService accountService) {
         this.postMapper = postMapper;
         this.accountMapper = accountMapper;
         this.voteMapper = voteMapper;
         this.commentMapper = commentMapper;
         this.commentService = commentService;
+        this.accountService = accountService;
     }
 
     @GetMapping("/{postId}/comments")
@@ -45,6 +48,19 @@ public class CommentController {
                 .toList();
 
         DataResponse<List<CommentDTO>> dataResponse = new DataResponse<>(true, commentDTOS);
+        return ResponseEntity.ok(dataResponse);
+    }
+
+    @PostMapping("/{postId}/comments")
+    public ResponseEntity<DataResponse<CommentDTO>> addCommentToPost(@PathVariable String postId, @RequestBody AddCommentBodyDTO commentDTO) {
+        // Salvează comentariul folosind serviciul
+        OffsetDateTime createdAt = OffsetDateTime.now();
+        Comment comment = new Comment(UUID.randomUUID(), accountService.getCurrentAccount(), null,
+                UUID.fromString(postId), commentDTO.content(), false, 0, 0, createdAt, createdAt);
+
+        commentService.addCommentToPost(comment);
+
+        DataResponse<CommentDTO> dataResponse = new DataResponse<>(true, commentMapper.modelToDTO(comment));
         return ResponseEntity.ok(dataResponse);
     }
 
