a3a8b2d20b2d52e5c55d3173eafffb22d1ea5282
Darius Preda <dariuspreda02@gmail.com>
2025-07-30 14:52:02 +0300

    Added .vs directory to gitignore

---
+++ Processor/ImageProcessor/Common/Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public class Common
+    {
+        public static void GetCommonMessage()
+        {
+           Console.WriteLine("This is a common message from the Common namespace.");
+        }
+    }
+}
