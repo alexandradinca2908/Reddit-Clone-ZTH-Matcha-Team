7b3096228efb537220be2cd52206f9977a3f4403
Darius Preda <dariuspreda02@gmail.com>
2025-08-14 16:13:00 +0300

    Moved AWS login keys in a separate file

--- Backend/spring-backend/src/main/java/org/matcha/springbackend/config/AwsConfig.java
+++ Backend/spring-backend/src/main/java/org/matcha/springbackend/config/AwsConfig.java
@@ -3,12 +3,14 @@ package org.matcha.springbackend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@PropertySource("file:${aws.credentials.file.path}")
 public class AwsConfig {
 
     @Value("${aws.accessKeyId}")
